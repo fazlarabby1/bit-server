9fb02a3060561c222c71bb4d84e698309c378f9b
fazlarabby1 <fazlarabby41@gmail.com>
2023-01-22 13:12:06 +0600

    get endpoint

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+    {id: 1, name: "Md Saddam Hossain"},
+    {id: 2, name: "Rakin Afser"}
+]
 
 app.get('/', (req, res)=>{
     res.send('Bit server running');
 })
 
+app.get('/get_name',(req, res)=>{
+    res.send(users);
+})
 
 
 app.listen(port, ()=>{
